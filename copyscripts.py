577721c8eeb06d1a37cf0b56db02b95833003131
wrmay <rmay@pivotal.io>
2017-05-11 18:50:38 -0400

    modified copyscripts to work off of cluster definition

--- copyscripts.py
+++ copyscripts.py
@@ -1,10 +1,22 @@
+import json
 import subprocess
 
-ssh_options = ['-o', 'StrictHostKeyChecking=false', '-o','UserKnownHostsFile=/dev/null', '-i', './vagrant/id_rsa']
+ssh_options = ['-o', 'StrictHostKeyChecking=false', '-o','UserKnownHostsFile=/dev/null', '-q']
 
 if __name__ == '__main__':
-  for server in ['10.10.10.101','10.10.10.102']:
-     for script in ['gf.py','cluster.py','clusterdef.py','gemprops.py', 'cluster.json', 'gemfire-toolkit/target/gemfire-toolkit-N-runtime.tar.gz']:
-        subprocess.check_call(['scp'] + ssh_options + [script, 'vagrant@{0}:/home/vagrant/gemfire_cluster'.format(server)])
 
-     subprocess.check_call(['ssh'] + ssh_options + ['vagrant@{0}'.format(server),'tar', '-C', '/home/vagrant/gemfire_cluster','-xzf', '/home/vagrant/gemfire_cluster/gemfire-toolkit-N-runtime.tar.gz'])
+  with open('cluster.json','r') as f:
+      cluster_config = json.load(f)
+
+  cluster_home = cluster_config['global-properties']['cluster-home']
+  for name, server in cluster_config['hosts'].items():
+     host = server['ssh']['host']
+     user = server['ssh']['user']
+     key_file = server['ssh']['key-file']
+     for script in ['gf.py','cluster.py','clusterdef.py','gemprops.py', 'cluster.json', 'gemfire-toolkit/target/gemfire-toolkit-N-runtime.tar.gz']:
+        subprocess.check_call(['scp'] + ssh_options + ['-i',key_file,script, '{0}@{1}:{2}'.format(user,host,cluster_home)])
+
+     print 'copied cluster scripts and cluster config to {0} on {1}'.format(cluster_home, host)
+
+     subprocess.check_call(['ssh'] + ssh_options + ['-i',key_file,'{0}@{1}'.format(user,host),'tar', '-C', cluster_home ,'-xzf', '{0}/gemfire-toolkit-N-runtime.tar.gz'.format(cluster_home)])
+     print 'copied gemfire toolkit to {0} on {1}'.format(cluster_home, host)
